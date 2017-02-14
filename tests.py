886af788132f78d2698fcc41ed9056ce5afe009b
shaobo <shaobol@hupofintech.com>
2017-02-14 14:48:59 +0800

    test for pre-commit

--- tests.py
+++ tests.py
@@ -2,17 +2,17 @@ import app
 import unittest
 
 class FlaskTestCase(unittest.TestCase):
-	def setUp(self):
-		self.app = app.app.test_client()
-		print 'setUp'
+    def setUp(self):
+        self.app = app.app.test_client()
+        print 'setUp'
 
-	def tearDown(self):
-		print 'tearDown'
+    def tearDown(self):
+        print 'tearDown'
 
-	def test_index(self):
-		rv = self.app.get('/')
-		assert b'Hello, World!' in rv.data
+    def test_index(self):
+        rv = self.app.get('/')
+        assert b'Hello, World!' in rv.data
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
 
